34dac534dd7cacfb7af1c2741075babe346c7004
aeryilmaz <aeryilmaz@outlook.com>
2019-12-31 01:03:23 +0300

    refactor : company name refactor uniquely

--- src/main/java/com/finartz/ticketingapplication/entity/Company.java
+++ src/main/java/com/finartz/ticketingapplication/entity/Company.java
@@ -17,6 +17,7 @@ public class Company implements Serializable {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column(unique = true)
     private String name;
 
     private String country;
